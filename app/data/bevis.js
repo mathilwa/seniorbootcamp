03daa0799525dac576cf92535821b0b65f535216
mathildewarstad <mathildewarstad@bekk.no>
2017-11-17 21:12:16 +0100

    Satt opp bevisdata

---
+++ app/data/bevis.js
@@ -0,0 +1,26 @@
+export const bevis = [
+  {
+    type: 'institusjon',
+    navn: 'BEKK seniorhjem',
+    kommunenummer: '1111',
+    bevisnummer: '123',
+    gydigFra: '',
+    gyldigTil: '',
+  },
+  {
+    type: 'institusjon',
+    navn: 'BEKK seniorhjem',
+    kommunenummer: '1111',
+    bevisnummer: '123',
+    gydigFra: '',
+    gyldigTil: '',
+  },
+  {
+    type: 'institusjon',
+    navn: 'BEKK seniorhjem',
+    kommunenummer: '1111',
+    bevisnummer: '123',
+    gydigFra: '',
+    gyldigTil: '',
+  },
+];
